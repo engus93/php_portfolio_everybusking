7381fcd3fd910c3f504bc0b5873891ff33fffb92
Dudu <engus983@gmail.com>
2018-12-28 17:21:00 +0900

    18.12.28
    회원가입

--- sign_up_result.php
+++ sign_up_result.php
@@ -1,15 +1,29 @@
 <?php
-    echo "post 방식을 배워봅시다. <br />";
+$host = 'localhost';
+$user = 'root';
+$pw = 'Reg016260!!';
+$dbName = 'everybusking_db';
+$mysqli = mysqli_connect($host, $user, $pw, $dbName);
 
-    echo "id ............. {$_POST['id']} <br />";
-    echo "name ............. {$_POST[name]} <br />";
-    echo "password ............. {$_POST[pwd]} <br />";
-    echo "confirm password ............. {$_POST[pwd2]} <br />";
-    echo "phone number.......... {$_POST[m1]} -{$_POST[m2]}-{$_POST[m3]} <br />";
-    echo "sex ............. {$_POST[sex]} <br />";
-    echo "address ............. {$_POST[addr]} <br />";
-    echo "hobby / computer ............. {$_POST[com]} <br />";
-    echo "hobby / sports ............. {$_POST[sports]} <br />";
-    echo "hobby / shopping ............. {$_POST[shop]} <br />";
-    echo "hobby / movie ............. {$_POST[mov]} <br />";
+$id = $_POST['ID'];
+$password = $_POST['password'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$sex = $_POST['sex'];
+
+echo $id;
+
+//$signup = mysqli_query($mysqli, "INSERT INTO user_info_tb VALUES ('$id','$password','$name','$phone','$sex')");
+
+$sql = "insert into user_info_tb VALUES('$id','$password','$name','$phone','$sex')";
+
+echo $sql;
+
+if ($mysqli->query($sql)) {
+    echo "<a href=sign_in.html>회원가입 성공</a>";
+} else {
+    echo "<a href=sign_up.html>회원가입 실패</a>";
+}
+
+
 ?>
