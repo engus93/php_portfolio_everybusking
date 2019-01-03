3b73206968023cee1ac5bfe4a928dd25265ad7a1
Dudu <engus983@gmail.com>
2019-01-04 02:06:52 +0900

    19.01.02
    커뮤니티 읽는 페이지 삭제 완료

--- main.php
+++ main.php
@@ -31,68 +31,11 @@ session_start();
 
 <body>
 
-<!--사이드바-->
-<div id="wrapper" class="animate">
-    <nav class="navbar header-top fixed-top navbar-expand-lg navbar-dark bg-dark">
-        <span class="navbar-toggler-icon leftmenutrigger"></span>
-        <a class="navbar-brand" href="main.php"
-           style="font-family: 'Monoton', cursive; margin-left: 10px; font-size: 20px;">Every
-            Busking</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
-                aria-controls="navbarText"
-                aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav animate side-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="buskingteam.html" title="Dashboard"><i
-                                class="fas fa-users side_bar_img"></i>Busking Team<i
-                                class="fas fa-cube shortmenu animate"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="busking_zone.html" title="Cart"><i
-                                class="fas fa-map-marker-alt side_bar_img"></i>Busking Zone<i
-                                class="fas fa-cart-plus shortmenu animate"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="streaming.html" title="Comment"><i class="fas fa-video side_bar_img"></i>Streaming<i
-                                class="fas fa-comment shortmenu animate"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="community/community.php" title="Comment"><i class="fas fa-star side_bar_img"></i>Community<i
-                                class="fas fa-comment shortmenu animate"></i></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="concert.html" title="Comment"><i
-                                class="fas fa-compact-disc side_bar_img"></i>Concert<i
-                                class="fas fa-comment shortmenu animate"></i></a>
-                </li>
-            </ul>
-            <ul class="navbar-nav ml-md-auto d-md-flex">
-                <li class="nav-item">
-                    <?php if ($_SESSION == null) {
-                        echo '<a class="nav-link"></a>';
-                    } else {
-                        echo '<a class="nav-link">'.$_SESSION['name'] . " 님" . '</a>';
-                    } ?>
+<!--footer 로드-->
+<div class="main_nav"></div>
+<script>$(".main_nav").load("/public/main_nav.php");</script>
 
-                </li>
-                <li class="nav-item">
-                    <?php if ($_SESSION == null) {
-                        echo '<a class="nav-link" href="Sign/sign_in.html"><i class="fas fa-key"></i> Sign In</a>';
-                    } else {
-                        echo '<a class="nav-link" href="Sign/sign_out.php"><i class="fas fa-key"></i> Sign Out</a>';
-                    } ?>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fas fa-user"></i> Profile</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</div>
-
+<!--슬라이드 사진 메인-->
 <header style="margin-top: 0px">
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
@@ -350,13 +293,9 @@ session_start();
     </div>
 </div>
 
-<!-- Footer -->
-<footer class="py-5 bg-dark">
-    <div class="container">
-        <p class="m-0 text-center text-white my_font_logo">Every Busking</p>
-    </div>
-    <!-- /.container -->
-</footer>
+<!--footer 로드-->
+<div class="main_footer"></div>
+<script>$(".main_footer").load("/public/main_footer.html");</script>
 
 <!-- Bootstrap core JavaScript -->
 <script src="vendor/jquery/jquery.min.js"></script>
---
+++ public/main_nav.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+?>
+
+<!--사이드바-->
+<div id="wrapper" class="animate">
+    <nav class="navbar header-top fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <span class="navbar-toggler-icon leftmenutrigger"></span>
+        <a class="navbar-brand" href="main.php"
+           style="font-family: 'Monoton', cursive; margin-left: 10px; font-size: 20px;">Every
+            Busking</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+                aria-controls="navbarText"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav animate side-nav">
+                <li class="nav-item">
+                    <a class="nav-link menu_buskingteam" href="/buskingteam.html" title="Dashboard"><i
+                            class="fas fa-users side_bar_img"></i>Busking Team<i
+                            class="fas fa-cube shortmenu animate"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link menu_busking_zone" href="/busking_zone.html" title="Cart"><i
+                            class="fas fa-map-marker-alt side_bar_img"></i>Busking Zone<i
+                            class="fas fa-cart-plus shortmenu animate"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link menu_streaming" href="/streaming.html" title="Comment"><i class="fas fa-video side_bar_img"></i>Streaming<i
+                            class="fas fa-comment shortmenu animate"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link menu_community" href="/community/community.php" title="Comment"><i class="fas fa-star side_bar_img"></i>Community<i
+                            class="fas fa-comment shortmenu animate"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link menu_concert" href="/concert.html" title="Comment"><i
+                            class="fas fa-compact-disc side_bar_img"></i>Concert<i
+                            class="fas fa-comment shortmenu animate"></i></a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-md-auto d-md-flex">
+                <li class="nav-item">
+                    <?php if ($_SESSION == null) {
+                        echo '<a class="nav-link"></a>';
+                    } else {
+                        echo '<a class="nav-link">'.$_SESSION['name'] . " 님" . '</a>';
+                    } ?>
+
+                </li>
+                <li class="nav-item" style="color: #FBAA48 !important;">
+                    <?php if ($_SESSION == null) {
+                        echo '<a class="nav-link" href="/Sign/sign_in.html"><i class="fas fa-key"></i> Sign In</a>';
+                    } else {
+                        echo '<a class="nav-link" href="/Sign/sign_out.php"><i class="fas fa-key"></i> Sign Out</a>';
+                    } ?>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link menu_profile" href="#"><i class="fas fa-user"></i> Profile</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
