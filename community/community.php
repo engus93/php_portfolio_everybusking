1a541fd3306f909d1937829e91b2327cdddf1658
Dudu <engus983@gmail.com>
2019-01-05 14:47:58 +0900

    19.01.05
    커뮤니티 페이징 완료

--- community/community.php
+++ community/community.php
@@ -53,7 +53,30 @@
         <?php
         require_once "../db.php";
 
-        $sql = mq("select * from community_tb order by idx desc limit 0,12"); // board테이블에있는 idx를 기준으로 내림차순해서 5개까지 표시
+//        페이징
+        if(isset($_GET['page'])){
+            $page = $_GET['page'];
+        }else{
+            $page = 1;
+        }
+        $sql = mq("select * from community_tb");
+        $row_num = mysqli_num_rows($sql); //게시판 총 레코드 수
+        $list = 4; //한 페이지에 보여줄 개수
+        $block_ct = 5; //블록당 보여줄 페이지 개수
+
+        $block_num = ceil($page/$block_ct); // 현재 페이지 블록 구하기
+        $block_start = (($block_num - 1) * $block_ct) + 1; // 블록의 시작번호
+        $block_end = $block_start + $block_ct - 1; //블록 마지막 번호
+
+        $total_page = ceil($row_num / $list); // 페이징한 페이지 수 구하기
+        if($block_end > $total_page) $block_end = $total_page; //만약 블록의 마지박 번호가 페이지수보다 많다면 마지박번호는 페이지 수
+        $total_block = ceil($total_page/$block_ct); //블럭 총 개수
+        $start_num = ($page-1) * $list; //시작번호 (page-1)에서 $list를 곱한다.
+
+        $sql = mq("select * from community_tb order by idx desc limit $start_num, $list");
+
+//        끝
+//        $sql = mq("select * from community_tb order by idx desc limit 0,12"); // board테이블에있는 idx를 기준으로 내림차순해서 5개까지 표시
 
         $ss = "../img/busking_defualt.jpg";
 
@@ -91,29 +114,61 @@
 
 </div>
 
-<ul class="pagination justify-content-center" style="margin-top: 20px; margin-bottom: 20px">
-    <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">Previous</span>
-        </a>
-    </li>
-    <li class="page-item">
-        <a class="page-link" href="#">1</a>
-    </li>
-    <li class="page-item">
-        <a class="page-link" href="#">2</a>
-    </li>
-    <li class="page-item">
-        <a class="page-link" href="#">3</a>
-    </li>
-    <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-        </a>
-    </li>
-</ul>
+<!--페이징-->
+
+<div id="page_num">
+    <ul class="pagination justify-content-center" style="margin-top: 20px; margin-bottom: 20px">
+        <?php
+        
+        //이전
+        if($page <= 1) { //만약 page가 1보다 크거나 같다면 빈값
+            echo "<li class='page-item'>
+                    <a class='page-link' href='?page=$page' aria-label='Previous'>
+                        <span aria-hidden='true'>&laquo;</span>
+                        <span class='sr-only'>Previous</span>                    
+                    </a>
+                   </li>";
+        }else{
+            $pre = $page-1; //pre변수에 page-1을 해준다 만약 현재 페이지가 3인데 이전버튼을 누르면 2번페이지로 갈 수 있게 함
+            echo "<li class='page-item'>
+                    <a class='page-link' href='?page=$pre' aria-label='Previous'>
+                        <span aria-hidden='true'>&laquo;</span>
+                        <span class='sr-only'>Previous</span>                    
+                    </a>
+                   </li>";
+        }
+        
+        //숫자 표시
+        for($i=$block_start; $i<=$block_end; $i++){
+            //for문 반복문을 사용하여, 초기값을 블록의 시작번호를 조건으로 블록시작번호가 마지박블록보다 작거나 같을 때까지 $i를 반복시킨다
+            if($page == $i){ //만약 page가 $i와 같다면
+                echo "<li class='page-item'><a class='page-link' href='?page=$i'
+                style='color: #FBAA48; font-weight: bold'>$i</a></li>"; //현재 페이지에 해당하는 번호에 굵은 빨간색을 적용한다
+            }else{
+                echo "<li class='page-item'><a class='page-link' href='?page=$i'>$i</a></li>";
+            }
+        }
+
+        //다음
+        if($page > $total_block) { //만약 현재 블록이 블록 총개수보다 크거나 같다면 빈 값
+            echo "<li class='page-item'>
+                    <a class='page-link' href='?page=$page' aria-label='Next'>
+                        <span aria-hidden='true'>&raquo;</span>
+                        <span class='sr-only'>Next</span>
+                    </a>
+                   </li>";
+        }else{
+            $next = $page + 1; //next변수에 page + 1을 해준다.
+            echo "<li class='page-item'>
+                    <a class='page-link' href='?page=$next' aria-label='Next'>
+                        <span aria-hidden='true'>&raquo;</span>
+                        <span class='sr-only'>Next</span>                    
+                    </a>
+                   </li>";
+        }
+        ?>
+    </ul>
+</div>
 
 <div class="main_footer"></div>
 <script>$(".main_footer").load("/public/main_footer.html");</script>
