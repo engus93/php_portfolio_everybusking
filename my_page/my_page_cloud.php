49e8a75d83c890bd6b61bf5832c5aab14b138fb0
Dudu <engus983@gmail.com>
2019-01-12 22:48:15 +0900

    19.01.11
    스트리밍 시작

--- my_page/my_page_cloud.php
+++ my_page/my_page_cloud.php
@@ -73,7 +73,7 @@ session_start();
         </li>
 
         <li class="breadcrumb-item">
-            <a  href="my_page_application.php" style="color: black">신청 내역</a>
+            <a href="my_page_application.php" style="color: black">신청 내역</a>
         </li>
 
         <li class="breadcrumb-item">
@@ -83,61 +83,129 @@ session_start();
     </ol>
 
     <!-- Page Heading/Breadcrumbs -->
-    <h4 class="my_font_main" style="margin-top: 50px;">팀노바 님의 펀딩 진행 사항</h4>
+    <h4 class="my_font_main" style="margin-top: 50px;">펀딩 진행 사항</h4>
 
-    <div style="position: relative;margin-left:45px; margin-top: 50px">
+    <div style="position: relative;margin-left:15px; margin-top: 50px">
         <!-- /.container -->
         <table class="list-table">
 
             <thead>
             <tr>
-                <th width="100">번호</th>
-                <th width="550">희망 날짜 / 신청 내용</th>
-                <th width="120">신청 종류</th>
-                <th width="150">작성일</th>
-                <th width="100">여부</th>
+                <th width="50">번호</th>
+                <th width="100">성함</th>
+                <th width="100">후원 내역</th>
+                <th width="230">선택 옵션</th>
+                <th width="120">후원 금액</th>
+                <th width="400">수령 방법 & 배송지</th>
+                <th width="120">관리</th>
+                <th width="80">진행 상황</th>
             </tr>
             </thead>
 
             <tbody>
+            <?php
+            require_once "../db.php";
 
-            <tr>
-                <td width=100">4</td>
-                <td width="550">19년 01월 05일 8시 / 공연 라이브 스트리밍 신청합니다.</a></td>
-                <td width="120">스트리밍 방송</td>
-                <td width="150">2019-01-02</td>
-                <td width="100">대기</td>
-            </tr>
+            //        페이징
+            if (isset($_GET['page'])) {
+                $page = $_GET['page'];
+            } else {
+                $page = 1;
+            }
+            $sql = mq("select * from payment_tb where name = '".$_SESSION['name']."'");
+            $row_num = mysqli_num_rows($sql); //게시판 총 레코드 수
+            $list = 10; //한 페이지에 보여줄 개수
+            $block_ct = 5; //블록당 보여줄 페이지 개수
 
-            <tr>
-                <td width=100">3</td>
-                <td width="550">18년 12월 29일 8시 / 공연 라이브 스트리밍 신청합니다.</a></td>
-                <td width="120">스트리밍 방송</td>
-                <td width="150">2018-12-24</td>
-                <td width="100">승인</td>
-            </tr>
+            $block_num = ceil($page / $block_ct); // 현재 페이지 블록 구하기
+            $block_start = (($block_num - 1) * $block_ct) + 1; // 블록의 시작번호
+            $block_end = $block_start + $block_ct - 1; //블록 마지막 번호
 
-            <tr>
-                <td width=100">2</td>
-                <td width="550">18년 12월 22일 8시 / 공연 라이브 스트리밍 신청합니다.</a></td>
-                <td width="120">스트리밍 방송</td>
-                <td width="150">2018-12-18</td>
-                <td width="100">승인</td>
-            </tr>
+            $total_page = ceil($row_num / $list); // 페이징한 페이지 수 구하기
+            if ($block_end > $total_page) $block_end = $total_page; //만약 블록의 마지박 번호가 페이지수보다 많다면 마지박번호는 페이지 수
+            $total_block = ceil($total_page / $block_ct); //블럭 총 개수
+            $start_num = ($page - 1) * $list; //시작번호 (page-1)에서 $list를 곱한다.
 
+            //        끝
+
+            $sql = mq("select COUNT(*) from payment_tb where name = '".$_SESSION['name']."'");
+
+            $num = mysqli_fetch_array($sql);
+
+            $num = ($num[0] + 1) - ($page - 1) * $list;
+
+            $sql = mq("select * from payment_tb where name = '".$_SESSION['name']."' order by idx desc limit $start_num, $list");
+
+            while ($board = $sql->fetch_array()) {
+
+            $sql = mq("select * from concert_tb where name = '".$board['busking_team']."'");
+
+            $board_re = $sql -> fetch_array();
+
+            $num = $num - 1;
+
+            ?>
             <tr>
-                <td width=100">1</td>
-                <td width="550">18년 12월 15일 8시 / 공연 라이브 스트리밍 신청합니다.</a></td>
-                <td width="120">스트리밍 방송</td>
-                <td width="150">2018-12-12</td>
-                <td width="100">승인</td>
+                <td width="50"><?= $num ?></td>
+                <td width="100"><?= $board['name'] ?></td>
+                <td width="100"><?= $board['busking_team'] ?></td>
+                <td width="230"><?= $board['product'] ?></td>
+                <td width="120"><?= $board['price'] ?></td>
+
+                <?php
+
+                if ($board['addr'] == "콘서트 당일 수령") {
+                    ?>
+
+                    <td width="400">콘서트 당일 현장 수령</td>
+                    <td width="120"><?= $board['etc'] ?></td>
+
+                    <?php
+                } else if ($board['etc'] == "배송 중") {
+                    ?>
+
+                    <td width="400">콘서트 당일 현장 수령</td>
+                    <td width="120"><?= $board['etc'] ?></td>
+
+                    <?php
+                } else {
+                    ?>
+
+                    <td width="400"><?= $board['post'] . "　/　" . $board['addr'] ?></td>
+                    <td width="120">
+                        <form style="display: inline" method="post" action="/my_page/manager_concert_punding_p.php">
+                            <input type="hidden" name="idx" value="<?= $board['idx'] ?>">
+                            <input type="hidden" name="page" value="<?= $page ?>">
+                            <button type="submit" id="support_hover" class="btn hover_class"
+                                    style="font-size: 14px; padding: 3px 7px 3px 7px">
+                                배송 시작 전
+                            </button>
+                        </form>
+                    </td>
+
+                    <?php
+                }
+
+                ?>
+
+                <td width="50">
+                    <form style="display: inline" method="post" action="/concert/concert_information.php?idx=<?= $board_re['idx'] ?>">
+                        <input type="hidden" name="page" value="<?= $page ?>">
+                        <button type="submit" id="support_hover" class="btn hover_class"
+                                style="font-size: 14px; padding: 3px 7px 3px 7px">
+                            보러가기
+                        </button>
+                    </form>
+                </td>
+
+                <?php
+                }
+                ?>
+
             </tr>
             </tbody>
-
         </table>
-
     </div>
-
 </div>
 
 <!--footer 로드-->
