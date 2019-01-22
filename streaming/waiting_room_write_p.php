492e3814b6ac75569be6586e9760cdbab8be8c99
Dudu <engus983@gmail.com>
2019-01-22 11:03:03 +0900

    19.01.22
    발표날

--- streaming/waiting_room_write_p.php
+++ streaming/waiting_room_write_p.php
@@ -12,24 +12,26 @@ date_default_timezone_set("Asia/SEOUL");
 
 $date = date('Y-m-d H:i:s');
 
-if (!empty($_FILES['c_file']['tmp_name'])) {
-    $tmpfile = $_FILES['c_file']['tmp_name'];
-    $o_name = $date.$_FILES['c_file']['name'];
-    $folder = "../img/streaming/";
-    $path = "$folder/$o_name";
+//if (!empty($_FILES['c_file']['tmp_name'])) {
+//    $tmpfile = $_FILES['c_file']['tmp_name'];
+//    $o_name = $date.$_FILES['c_file']['name'];
+//    $folder = "../img/streaming/";
+//    $path = "$folder/$o_name";
+//
+//    //폴더 체크 후 생성
+//    if (!is_dir($folder)) {
+//        mkdir($folder);
+//    }
+//
+//    move_uploaded_file($_FILES['c_file']['tmp_name'], "$folder/$o_name");
+//
+//} else {
+//
+//    $path = '/img/streaming_default.gif';
+//
+//}
 
-    //폴더 체크 후 생성
-    if (!is_dir($folder)) {
-        mkdir($folder);
-    }
-
-    move_uploaded_file($_FILES['c_file']['tmp_name'], "$folder/$o_name");
-
-} else {
-
-    $path = '/img/streaming_default.gif';
-
-}
+$path = $_SESSION['profile'];
 
 $sql = mq("insert into streaming_tb(streamer,streamer_id,date,picture) values('" . $title . "','" . $user_id . "','" . $date . "','" . $path . "')");
 
